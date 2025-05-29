04066e4268229824632398407b5275e0e6fd4a88
bobdreads <mauriciofilho60@gmail.com>
2025-05-28 21:40:09 -0300

    Tabela finalizada

--- dash/admin.py
+++ dash/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .models import *
+
+# Classe de Admin para Operacao (NOVO)
+
+
+class OperacaoAdmin(admin.ModelAdmin):
+    list_display = (
+        'ativo',
+        'lado',
+        'qtd',
+        'preco_medio',
+        'total_executado',
+        'user',  # Mostra o __str__ do CustomUser
+        'ultima_atualizacao',
+    )
+    list_filter = (
+        'user',  # Filtra por utilizador
+        'lado',   # Filtra por Compra/Venda
+        'ativo',
+        'corretora',
+        'ultima_atualizacao'
+    )
+    search_fields = (
+        'ativo',
+        'user__username',  # Permite pesquisar pelo username do utilizador associado
+        'corretora',
+        'conta',
+    )
+    # Adiciona uma navegação hierárquica por data
+    date_hierarchy = 'ultima_atualizacao'
+    # Campos que não podem ser editados na admin
+
+
+admin.site.register(Operacao, OperacaoAdmin)
